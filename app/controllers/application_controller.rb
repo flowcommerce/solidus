cd566ba3536ae7ad12680a102a1a2057f815167c
David Jones <dave@resolvedigital.com>
2016-08-26 07:28:39 -0700

    Flow Fashion Theme (#2)
    * add Bourbon and Neat. converted grid system from Skeleton to work with Neat
    
    * better use of grid system. added variables file to be a bit more like bourbon. setup linting for sass
    
    * split css out into descrete files. improve grid layout to be better aligned. tweaks to the header
    
    * improvements to the product detail page
    
    * keep the linter happy
    
    * allow side menu to open and close
    
    * spacing
    
    * always render sidebar taxonomies
    
    * layout the checkout better
    
    * fonts, colour updates, minor ui tweaks
    
    * force ssl
    
    * tidy up spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :find_taxonomies
+
+  private
+
+  def find_taxonomies
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
 end
