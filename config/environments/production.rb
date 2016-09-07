7316e6fbf008b671efd4ee70b565f44769189278
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-09-07 14:19:52 +1200

    Adjusted S3 credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,11 +92,11 @@ Rails.application.configure do
   Paperclip::Attachment.default_options.merge!(
     storage: :s3,
     s3_protocol: :https,
+    s3_region: ENV["S3_REGION"],
     s3_credentials: {
       bucket: ENV["S3_BUCKET_NAME"],
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      s3_region: ENV["S3_REGION"]
     }
   )
 end
