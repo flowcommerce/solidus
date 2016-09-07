78615bb59a6c225dd9841d0967ed413651d5d558
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-09-07 14:11:37 +1200

    Updated S3 credentials
    Current version of aws-sdk gem requires that S3 region be specified.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,14 @@ Rails.application.configure do
   }
 
   # Use S3 for asset storage
-  Paperclip::Attachment.default_options.merge!({
-    :storage => :s3,
-    :s3_protocol => :https,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  Paperclip::Attachment.default_options.merge!(
+    storage: :s3,
+    s3_protocol: :https,
+    s3_credentials: {
+      bucket: ENV["S3_BUCKET_NAME"],
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      s3_region: ENV["S3_REGION"]
     }
-  })
+  )
 end
