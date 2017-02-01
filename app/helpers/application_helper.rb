10827efc1c9b9fadbb1de5144787c7c2377034d7
Dino Reic <rejotl@gmail.com>
2017-02-01 20:45:13 +0100

    dev: frontend design changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Ads markdown rendering to product descripton
+  #
+  # @param product [Spree::Product] the product whose description you want to filter
+  # @return [String] the generated HTML
+  def flow_product_description(product)
+    data = ' '+raw(product.description.gsub(/(.*?)\r?\n\r?\n/m, '<p>\1</p>'))
+    data.gsub!(' *', '<li>')
+    data.html_safe
+  end
 end
