bb715cfc5c93afccbc44c162be984f4943b0e84a
Dino Reic <rejotl@gmail.com>
2017-03-07 20:31:40 +0100

    dev: Added controller method filters, better product price render and removed product cacheing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,29 +16,35 @@ class ApplicationController < ActionController::Base
     @flow_exp = FlowExperience.init_by_key(session[:flow_exp_key]) || Flow.experiences.first
   end
 
-  # we need to prepare @order and sync to flow.io before render because we need
-  # flow total price
-  def sync_flow_order
-    # target = '%s#%s' % [params[:controller], params[:action]]
-    # if ['spree/checkout#edit','spree/orders#edit'].include?(target)
-
-    # ap @product.variants.first.flow_cache
-
-    # r @flow_exp.get_item @product
-
-    # @product.variants.first.update_column :flow_cache, nil
-
-
-    return unless @order
-
-    FlowOrder.sync_from_spree_order(experience: @flow_exp, order: @order, customer: @current_spree_user)
-  end
-
   # before render trigger
   # rails does not have before_render filter so we create it like this
   # to make things simple
   def render(*args)
-    [:sync_flow_order].each { |action| send action }
-    super
+    # call method if one defined
+    target        = '%s#%s' % [params[:controller], params[:action]]
+    filter_method = ('flow_filter_' + target.gsub!(/[^\w]/,'_')).to_sym
+    send(filter_method) if self.class.private_instance_methods.include?(filter_method)
+
+    # call all this methods
+    sync_flow_order
+
+    # return our data or call super render
+    @flow_render ? super(@flow_render) : super
+  end
+
+  ###
+
+  # we need to prepare @order and sync to flow.io before render because we need
+  # flow total price
+  def sync_flow_order
+    return unless @order
+    flow_order = FlowOrder.sync_from_spree_order(experience: @flow_exp, order: @order, customer: @current_spree_user)
+    @flow_render = { json: flow_order.to_json } if params[:debug] == 'flow'
+  end
+
+  def flow_filter_spree_products_show
+    # r @product.variants.first.flow_cache
+    @flow_render = { json: @flow_exp.get_item(@product).to_json } if params[:debug] == 'flow'
+    # @product.variants.first.update_column :flow_cache, nil
   end
 end
