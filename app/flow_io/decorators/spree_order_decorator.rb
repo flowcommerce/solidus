63cc6656ce8b29e32a60324da26d348157ebe046
Dino Reic <rejotl@gmail.com>
2017-04-24 19:58:47 +0200

    dev: Match flow number with solidus Order ID (CH_ID: 2410)

--- app/flow_io/decorators/spree_order_decorator.rb
+++ app/flow_io/decorators/spree_order_decorator.rb
@@ -5,18 +5,10 @@ require 'digest/sha1'
 
 Spree::Order.class_eval do
 
-  # defines uniqe flow number per order
-  # format "solidus-order-hash" -> "s-o-hash"
+  # we now use Solidus number as Flow number, but I will this here for now
   def flow_number
     return self[:flow_number] unless self[:flow_number].blank?
     return unless id
-
-    token = ENV.fetch('SECRET_TOKEN')
-    number = 's-o-%s' % Digest::SHA1.hexdigest('%d-%s' % [id, token])
-
-    # fast update without callbacks
-    update_column :flow_number, number
-
     number
   end
 
