74dad4f61431a25e66c8dba7098c2684b68c27b6
Dino Reic <rejotl@gmail.com>
2017-08-09 18:39:48 +0200

    master: better deployer and remote access

--- app/assets/javascripts/spree/frontend/popup.js
+++ app/assets/javascripts/spree/frontend/popup.js
@@ -1,3 +1,4 @@
+// updates current order in custom flow route
 window.CurrentOrder = {
   update: function(name, value) {
     if (window.in_reload) { return; }
@@ -18,6 +19,7 @@ window.CurrentOrder = {
   },
 }
 
+// popup is only visible for delivery options
 window.Popup = {
   open:  function() {
     var popup = $('#popup');
