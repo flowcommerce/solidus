9238bffce75ac11445daf162c00b8ed255e4145c
Dino Reic <rejotl@gmail.com>
2017-11-17 01:16:08 +0100

    master: S3 asset host preparation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,10 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  rails_asset_host = ENV['RAILS_ASSET_HOST']
+  if rails_asset_host
+    config.action_controller.asset_host = rails_asset_host
+  end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
