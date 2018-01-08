13b131f9e139ccf5731ce465ae20bbbb576d5621
Dino Reic <rejotl@gmail.com>
2018-01-08 17:43:47 +0100

    master: only allow coupon code in experience that is enabled

--- app/flow/decorators/localized_coupon_code_decorator.rb
+++ app/flow/decorators/localized_coupon_code_decorator.rb
@@ -10,17 +10,9 @@ Spree::Api::OrdersController.class_eval do
     promotion_code = Spree::PromotionCode.find_by value: coupon_code
 
     if promotion_code
-      # promotion code found
       promotion       = Spree::Promotion.find promotion_code.promotion_id
-
-      # get experience key from session
-      experience_data = JSON.load(session[ApplicationController::FLOW_SESSION_KEY] || '{}')
-
-      experience_key  = experience_data.dig 'local', 'experience', 'key'
+      experience_key  = @order.flow_order.dig('experience', 'key')
       forbiden_keys   = promotion.flow_data.dig 'filter', 'experience'
-
-      # Rails.logger.error 'experience_key: %s' % experience_key
-      # Rails.logger.error 'forbiden_keys: %s' % forbiden_keys.join(', ')
 
       allowed = true
       allowed = false if experience_key && forbiden_keys.is_a?(Array) && !forbiden_keys.include?(experience_key)
@@ -37,8 +29,10 @@ Spree::Api::OrdersController.class_eval do
     # apply_coupon_code_pointer
 
     authorize! :update, @order, order_token
+
     @order.coupon_code = params[:coupon_code]
     @handler = Spree::PromotionHandler::Coupon.new(@order).apply
+
     if @handler.successful?
       render "spree/api/promotions/handler", status: 200
     else
