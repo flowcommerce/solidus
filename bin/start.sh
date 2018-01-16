d6ce41464c6ff6f5d91f9ce2cad27bb3e7db42c5
Dino Reic <rejotl@gmail.com>
2018-01-16 22:33:06 +0100

    increased warmup time for sync

--- bin/start.sh
+++ bin/start.sh
@@ -9,13 +9,13 @@ puma -p 3000 -e production &
 while true
 do
   # sleep first, refresh later
-  sleep 1000
+  sleep 2000
 
   # refresh DB
-  rake flow:sync_localized_items
+  bundle exec rake flow:sync_localized_items
 
   # inform that refresh happend
   curl -fsS --retry 3 https://hchk.io/93912c0e-65cd-4f5b-a912-8983448f370b > /dev/null
 
-  sleep 3000
+  sleep 2000
 done
